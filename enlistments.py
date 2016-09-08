9810368ff1d1460dc90f8637d5ed7036fd4ac0b1
Abdullatif Al-Zaidan <abdullatifalzaidan@gmail.com>
2016-09-08 15:42:37 +0300

    updated the script

--- enlistments.py
+++ enlistments.py
@@ -5,6 +5,26 @@ import urllib.request
 from common import fetch_df_from_xml
 from settings import MSG_FOLDER
 
+urls = []
+repo_type = []
+
+
+def xml_beutifulSoup():
+    url = "https://www.openhub.net/p/641496/enlistments"
+    content = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(content)
+    soup.findAll('table')[0].tbody.findAll('tr')
+    for row in soup.findAll('table')[0].tbody.findAll('tr'):
+        url = row.findAll('td')[0].text.strip()
+        repo = row.findAll('td')[1].text.strip()
+        urls.append(url)
+        repo_type.append(repo)
+    # we need the ID as well.
+    data = {"URL": urls, "repo_type": repo_type, "ID": pid}
+    df = pd.DataFrame(data)
+    print("### enlistments DF: ")
+    print(df)
+
 merged_contribs = []
 for k in range(1, 21):
     projects_df = fetch_df_from_xml("projects", page=k)
@@ -12,27 +32,11 @@ for k in range(1, 21):
         # for pid in project_df.id:
         for pid in projects_df.id:
             merged_contribs.append(
-                fetch_df_from_xml("enlistment", pid=pid)
-                )
-    except urllib.error.HTTPError as err: #is it cocrrect way?
-        #How do we give the function (fetch_df_from_xml) the url, which returned the 404?
-        #and then, where do we contruct the DataFrame? becuase it'll be different than the contributors
-        fetch_df_from_xml("enlistment")
-        url = "https://www.openhub.net/p/641496/enlistments"
-        content = urllib.request.urlopen(url).read()
-        soup = BeautifulSoup(content)
-        urls = []
-        repo_type = []
-        soup.findAll('table')[0].tbody.findAll('tr')
-        for row in soup.findAll('table')[0].tbody.findAll('tr'):
-            url = row.findAll('td')[0].text.strip()
-            repo = row.findAll('td')[1].text.strip()
-            urls.append(url)
-            repo_type.append(repo)
-        data = {"URL": urls, "repo_type": repo_type} #we need the ID as well.
-        df = pd.DataFrame(data)
-        print("### enlistments DF: ")
-        print(df)
+                fetch_df_from_xml("enlistments", pid=pid)
+            )
+    except urllib.error.HTTPError as err:
+        print(err)
+        #xml_beutifulSoup()
 
 
 
