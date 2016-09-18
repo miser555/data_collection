431937b8cf9ebf20837b0163b20e02de0bdb2131
Mohammad AlMarzouq <m.almarzouq@kbsoft.com.kw>
2016-09-18 12:51:31 +0300

    fixed the enlistment fetching, you need to make it work for multiple pages

--- enlistments.py
+++ enlistments.py
@@ -9,10 +9,11 @@ urls = []
 repo_type = []
 
 
-def xml_beutifulSoup():
-    url = "https://www.openhub.net/p/641496/enlistments"
+def fetch_enlistment_from_html(pid):
+    # use parameters in function names
+    url = "https://www.openhub.net/p/{}/enlistments".format(pid)
     content = urllib.request.urlopen(url).read()
-    soup = BeautifulSoup(content)
+    soup = BeautifulSoup(content, "lxml") # set the parser as lxml
     soup.findAll('table')[0].tbody.findAll('tr')
     for row in soup.findAll('table')[0].tbody.findAll('tr'):
         url = row.findAll('td')[0].text.strip()
@@ -20,27 +21,48 @@ def xml_beutifulSoup():
         urls.append(url)
         repo_type.append(repo)
     # we need the ID as well.
+    # if should have been passed from the function parameters
     data = {"URL": urls, "repo_type": repo_type, "ID": pid}
     df = pd.DataFrame(data)
     print("### enlistments DF: ")
-    print(df)
+    # do not forget to return the df!
+    return df
 
-merged_contribs = []
-for k in range(1, 21):
-    projects_df = fetch_df_from_xml("projects", page=k)
-    try:
+# use proper variable names, like merged_enlistments .. or _data
+merged_data = []
+
+
+# this if statement is to ensure
+# that the script is only executed from command line
+# this would allow us e to import the function for testing
+# without executing the script
+if __name__ == '__main__':
+
+    for k in range(1, 21):
+        projects_df = fetch_df_from_xml("projects", page=k)
         # for pid in project_df.id:
         for pid in projects_df.id:
-            merged_contribs.append(
-                fetch_df_from_xml("enlistments", pid=pid)
-            )
-    except urllib.error.HTTPError as err:
-        print(err)
-        #xml_beutifulSoup()
+            # the exception would occur only in
+            # fetch_df_from_xml,
+            # you still want to continue the loop
+            # if there is an error
+            # you also want to use pid to set the correctly
+            # url, see how I changed the indentation level for
+            # the try statement
+            try:
+                merged_data.append(
+                    fetch_df_from_xml("enlistments", pid=pid)
+                )
+            except urllib.error.HTTPError as err:
+                # your function did not return a df
+                # you have to return a df and then append it
+                # to merged_data
+                merged_data.append(
+                    fetch_enlistment_from_html(pid)
+                )
 
 
-
-merged_contribs = pd.concat(merged_contribs, axis=0)
-merged_df_file = os.path.join(MSG_FOLDER, "enlistments.msg")
-merged_contribs.to_msgpack(merged_df_file, encoding="utf-8")
-print("successfully merged everything into {}".format(merged_df_file))
+    merged_data = pd.concat(merged_data, axis=0)
+    merged_df_file = os.path.join(MSG_FOLDER, "enlistments.msg")
+    merged_data.to_msgpack(merged_df_file, encoding="utf-8")
+    print("successfully merged everything into {}".format(merged_df_file))
